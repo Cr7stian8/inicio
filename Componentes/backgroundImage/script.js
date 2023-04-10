c7e5d5cf97ae390de8e39c7a1212fbefd0b85622
Cr7stian8 <cristianotropeiro@gmail.com>
2023-04-10 17:28:21 -0300

    Mudando imagens de fundo

--- Componentes/backgroundImage/script.js
+++ Componentes/backgroundImage/script.js
@@ -11,13 +11,11 @@ const imagensDeFundo = [
   'assets/imgs/imagens-minimalistas/1.png',
   'assets/imgs/imagens-minimalistas/2.png',
   'assets/imgs/imagens-minimalistas/3.png',
+  'assets/imgs/imagens-minimalistas/4.jpeg',
+  'assets/imgs/imagens-minimalistas/5.webp',
 ];
 
 const indiceAleatorio = Math.floor(Math.random() * imagensDeFundo.length);
 
 const pagina = document.getElementById('pagina');
 pagina.style.backgroundImage = `url('${imagensDeFundo[indiceAleatorio]}')`;
-pagina.style.backgroundSize = 'cover'; // exemplo
-pagina.style.backgroundRepeat = 'no-repeat'; // exemplo
-pagina.style.backgroundPosition = 'center center'; // exemplo
-// 
