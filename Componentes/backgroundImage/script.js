d52ce6639b6417c16c69c79f2edb73b6cfd58f5b
Cr7stian8 <cristianotropeiro@gmail.com>
2023-04-13 19:13:46 -0300

    passando o pano em imagens que no deveriam existir

--- Componentes/backgroundImage/script.js
+++ Componentes/backgroundImage/script.js
@@ -10,8 +10,6 @@ thumbnails.forEach(thumbnail => {
 const imagensDeFundo = [
   'https://static.vecteezy.com/ti/vetor-gratis/p1/9169455-ceu-dourado-por-do-sol-na-costa-natureza-paisagem-vetor.jpg',
   'assets/imgs/imagens-minimalistas/2.png',
-  'https://static.vecteezy.com/ti/vetor-gratis/p1/4865260-minimalista-nebuloso-manha-montanha-paisagem-ilustracao-vetor.jpg',
-  'assets/imgs/imagens-minimalistas/4.jpeg',
   'assets/imgs/imagens-minimalistas/5.webp',
 ];
 
