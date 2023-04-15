7570d2c500c99ce14c329423f8829638bb7c9454
Cr7stian8 <cristianotropeiro@gmail.com>
2023-04-15 09:44:23 -0300

    alterações de estilo e animações

--- Componentes/navBar/script.js
+++ Componentes/navBar/script.js
@@ -4,3 +4,14 @@ var nav = document.querySelector('nav');
 menuIcon.addEventListener('click', function() {
   nav.classList.toggle('show');
 });
+
+
+const icone = document.querySelector('.menu-icon');
+icone.addEventListener('click', function() {
+  // Adicione ou remova a classe 'girado' do elemento
+  if (icone.classList.contains('girando')) {
+    icone.classList.remove('girando');
+  } else {
+    icone.classList.add('girando');
+  }
+});
