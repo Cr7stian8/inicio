06ed6efc81c268a872e45de9c09b178aa0c8b0b4
Cr7stian8 <cristianotropeiro@gmail.com>
2023-04-21 11:02:19 -0300

    adicionando Js pra ficar baala

---
+++ Componentes/PesquisarWeb/script.js
@@ -0,0 +1,20 @@
+const searchButton = document.querySelector('#barraBusca');
+const sessaoLinks = document.querySelector('#sessaoLinks');
+const thumb = document.querySelector('.thumbnail-section')
+const links = document.querySelector('#outrosProjetos')
+const imgGoogle = document.querySelector('#google')
+
+searchButton.addEventListener('focus', () => {
+    sessaoLinks.style.display = 'none';
+    thumb.style.display = 'none';
+    links.style.display = 'none';
+    imgGoogle.style.transition = '1000ms';
+    imgGoogle.style.transform = 'scale(0.9)';
+});
+searchButton.addEventListener('blur', () => {
+    sessaoLinks.style.display = 'block';
+    thumb.style.display = 'block';
+    links.style.display = 'block';
+    imgGoogle.style.transform = 'scale(1)';
+    imgGoogle.style.transition = '1000ms';
+});
