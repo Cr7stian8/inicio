66253b05283ef5d599479086f2fa6672c4f9b2fc
Cr7stian8 <cristianotropeiro@gmail.com>
2023-05-08 16:40:06 -0300

    chegaporhj

--- Componentes/PesquisarWeb/script.js
+++ Componentes/PesquisarWeb/script.js
@@ -19,3 +19,24 @@ searchButton.addEventListener('blur', () => {
     imgGoogle.style.transform = 'scale(1)';
     imgGoogle.style.transition = '1000ms';
 });
+
+// Verifica se o cookie 'visitas' existe
+if (document.cookie.split(';').some((item) => item.trim().startsWith('visitas='))) {
+    // Se o cookie existe, recupera seu valor e incrementa
+    const visitasAnteriores = parseInt(getCookie('visitas'));
+    document.cookie = 'visitas=' + (visitasAnteriores + 1);
+  } else {
+    // Se o cookie não existe, cria um novo com valor 1
+    document.cookie = 'visitas=1';
+  }
+
+  // Função para obter o valor de um cookie pelo seu nome
+  function getCookie(nome) {
+    const valor = `; ${document.cookie}`;
+    const partes = valor.split(`; ${nome}=`);
+    if (partes.length === 2) return partes.pop().split(';').shift();
+  }
+  
+  const visitas = getCookie('visitas');
+  const visitasSpan = document.getElementById('visitas');
+  visitasSpan.textContent = visitas;
--- Componentes/navBar/script.js
+++ Componentes/navBar/script.js
@@ -1,14 +1,13 @@
 var menuIcon = document.querySelector('.menu-icon');
 var nav = document.querySelector('nav');
 
-menuIcon.addEventListener('click', function () {
+menuIcon.addEventListener('mouseenter', function () {
   nav.classList.toggle('show');
 });
 
 
 const icone = document.querySelector('.menu-icon');
-icone.addEventListener('click', function () {
-  // Adicione ou remova a classe 'girado' do elemento
+icone.addEventListener('mouseenter', function () {
   if (icone.classList.contains('girando')) {
     icone.classList.remove('girando');
   } else {
